6869afbe4ada885badbaf6d9c49f36c8b1e67b77
fnaufalh <fnaufalh@gmail.com>
2023-03-08 09:01:46 +0700

    change template price

--- code.ts
+++ code.ts
@@ -1,31 +1,73 @@
-// This plugin creates 5 rectangles on the screen.
-// const numberOfRectangles = 5
+// figma.showUI(__html__, { width: 320, height: 240 });
 
-// This file holds the main code for the plugins. It has access to the *document*.
-// You can access browser APIs such as the network by creating a UI which contains
-// a full browser environment (see documentation).
+// // When the plugin UI is shown, load the `bundle.js` file
+// figma.ui.onready(() => {
+//   fetch("index.html")
+//     .then((response) => response.text())
+//     .then((html) => {
+//       const parser = new DOMParser();
+//       const doc = parser.parseFromString(html, "text/html");
+//       const script = doc.createElement("script");
+//       script.src = "bundle.js";
+//       doc.body.appendChild(script);
+//       document.body.innerHTML = doc.body.innerHTML;
+//     })
+//     .catch((error) => console.error(error));
+// });
 
-const selection = figma.currentPage.selection;
-const page = figma.currentPage;
-const nodes: SceneNode[] = [];
-// for (let i = 0; i < numberOfRectangles; i++) {
-//   const rect = figma.createRectangle();
-//   rect.x = i * 150;
-//   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-//   figma.currentPage.appendChild(rect);
-//   nodes.push(rect);
-// }
-selection.forEach((item) => {
-  if (item.parent?.type === "PAGE") {
-    page.appendChild(item);
-    nodes.push(item);
+// const formatter = new Intl.NumberFormat("en-US", {
+//   style: "currency",
+//   currency: "USD",
+// });
+
+const changePrice = async () => {
+  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
+  await figma.loadFontAsync({ family: "Inter", style: "Medium" });
+  const selection = figma.currentPage.selection;
+  const page = figma.currentPage;
+  const nodes: SceneNode[] = [];
+  // for (let i = 0; i < numberOfRectangles; i++) {
+  //   const rect = figma.createRectangle();
+  //   rect.x = i * 150;
+  //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+  //   figma.currentPage.appendChild(rect);
+  //   nodes.push(rect);
+  // }
+  // console.log(formatter.format(20000));
+  if (selection.length > 0) {
+    let instanceSet = selection.find(
+      (node) => node.type === "INSTANCE"
+    ) as InstanceNode;
+    console.log(instanceSet);
+    const templateStrikePrice = instanceSet.findOne(
+      (node) => node.name === "Strike Price" && node.type === "TEXT"
+    ) as TextNode;
+    const templatePrice = instanceSet.findOne(
+      (node) => node.name === "Price" && node.type === "TEXT"
+    ) as TextNode;
+
+    templateStrikePrice.characters =
+      "Rp" +
+      // formatter.format(
+      Math.floor(Math.random() * 1000000) +
+      (1)
+        // )
+        .toString();
+    templatePrice.characters =
+      "Rp" +
+      // formatter.format(
+      Math.floor(Math.random() * 1000000) +
+      (1)
+        // )
+        .toString();
+    console.log(templateStrikePrice, templateStrikePrice);
+    // nodes.push();
+    // figma.viewport.scrollAndZoomIntoView(nodes);
+    figma.closePlugin();
   } else {
-    console.log(item.parent?.type);
+    figma.closePlugin("Please make a selection");
   }
-});
-console.log(nodes);
-figma.viewport.scrollAndZoomIntoView(nodes);
-
+};
+changePrice();
 // Make sure to close the plugin when you're done. Otherwise the plugin will
 // keep running, which shows the cancel button at the bottom of the screen.
-figma.closePlugin();
