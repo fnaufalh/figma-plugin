781d2239a6345dab95a524613278a5663513ce5b
fnaufalh <fnaufalh@gmail.com>
2023-03-08 07:14:27 +0700

    init

---
+++ code.ts
@@ -0,0 +1,31 @@
+// This plugin creates 5 rectangles on the screen.
+// const numberOfRectangles = 5
+
+// This file holds the main code for the plugins. It has access to the *document*.
+// You can access browser APIs such as the network by creating a UI which contains
+// a full browser environment (see documentation).
+
+const selection = figma.currentPage.selection;
+const page = figma.currentPage;
+const nodes: SceneNode[] = [];
+// for (let i = 0; i < numberOfRectangles; i++) {
+//   const rect = figma.createRectangle();
+//   rect.x = i * 150;
+//   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
+//   figma.currentPage.appendChild(rect);
+//   nodes.push(rect);
+// }
+selection.forEach((item) => {
+  if (item.parent?.type === "PAGE") {
+    page.appendChild(item);
+    nodes.push(item);
+  } else {
+    console.log(item.parent?.type);
+  }
+});
+console.log(nodes);
+figma.viewport.scrollAndZoomIntoView(nodes);
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
