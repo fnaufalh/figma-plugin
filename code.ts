596a3859a2aaf293be15b818cf5bdf97fc843a14
Fathin Naufal Hawi <naufal@Fathins-MacBook-Air.local>
2023-03-13 20:11:26 +0700

    scaling success, flatten error

--- code.ts
+++ code.ts
@@ -1,73 +1,50 @@
-// figma.showUI(__html__, { width: 320, height: 240 });
+const SIZE = [24, 16, 12];
+const select = figma.currentPage.selection;
+const selectedVectors: any = [];
 
-// // When the plugin UI is shown, load the `bundle.js` file
-// figma.ui.onready(() => {
-//   fetch("index.html")
-//     .then((response) => response.text())
-//     .then((html) => {
-//       const parser = new DOMParser();
-//       const doc = parser.parseFromString(html, "text/html");
-//       const script = doc.createElement("script");
-//       script.src = "bundle.js";
-//       doc.body.appendChild(script);
-//       document.body.innerHTML = doc.body.innerHTML;
-//     })
-//     .catch((error) => console.error(error));
-// });
-
-// const formatter = new Intl.NumberFormat("en-US", {
-//   style: "currency",
-//   currency: "USD",
-// });
-
-const changePrice = async () => {
-  await figma.loadFontAsync({ family: "Inter", style: "Regular" });
-  await figma.loadFontAsync({ family: "Inter", style: "Medium" });
-  const selection = figma.currentPage.selection;
-  const page = figma.currentPage;
-  const nodes: SceneNode[] = [];
-  // for (let i = 0; i < numberOfRectangles; i++) {
-  //   const rect = figma.createRectangle();
-  //   rect.x = i * 150;
-  //   rect.fills = [{type: 'SOLID', color: {r: 1, g: 0.5, b: 0}}];
-  //   figma.currentPage.appendChild(rect);
-  //   nodes.push(rect);
-  // }
-  // console.log(formatter.format(20000));
-  if (selection.length > 0) {
-    let instanceSet = selection.find(
-      (node) => node.type === "INSTANCE"
-    ) as InstanceNode;
-    console.log(instanceSet);
-    const templateStrikePrice = instanceSet.findOne(
-      (node) => node.name === "Strike Price" && node.type === "TEXT"
-    ) as TextNode;
-    const templatePrice = instanceSet.findOne(
-      (node) => node.name === "Price" && node.type === "TEXT"
-    ) as TextNode;
-
-    templateStrikePrice.characters =
-      "Rp" +
-      // formatter.format(
-      Math.floor(Math.random() * 1000000) +
-      (1)
-        // )
-        .toString();
-    templatePrice.characters =
-      "Rp" +
-      // formatter.format(
-      Math.floor(Math.random() * 1000000) +
-      (1)
-        // )
-        .toString();
-    console.log(templateStrikePrice, templateStrikePrice);
-    // nodes.push();
-    // figma.viewport.scrollAndZoomIntoView(nodes);
-    figma.closePlugin();
-  } else {
-    figma.closePlugin("Please make a selection");
+function selectAllVector(component: any) {
+  if (component.type === "VECTOR") {
+    console.log(component.children);
+    for (const child of component.children) {
+      selectedVectors.push(child);
+    }
   }
-};
-changePrice();
-// Make sure to close the plugin when you're done. Otherwise the plugin will
-// keep running, which shows the cancel button at the bottom of the screen.
+  figma.currentPage.selection = selectedVectors;
+}
+
+function iconScaling() {
+  for (const node of select) {
+    if (node.type === "COMPONENT_SET") {
+      const componentSet = select.find(
+        (node) => node.type === "COMPONENT_SET"
+      ) as ComponentSetNode;
+      componentSet.layoutMode = "VERTICAL";
+      componentSet.layoutAlign = "STRETCH";
+      const component = componentSet.findOne(
+        (node) => node.type === "COMPONENT"
+      ) as ComponentNode;
+      selectAllVector(component);
+      // const vector = component.findOne(
+      //   (node) => node.type === "VECTOR"
+      // ) as VectorNode;
+      // vector.constraints = {
+      //   horizontal: "SCALE",
+      //   vertical: "SCALE",
+      // };
+      // for (const size of SIZE) {
+      //   const clone = component.clone() as ComponentNode;
+      //   const grid = clone.findOne(
+      //     (node) => node.type === "INSTANCE" && node.name === "grid-icon"
+      //   ) as SceneNode;
+      //   grid?.remove();
+      //   clone.resize(size, size);
+      //   componentSet.appendChild(clone);
+      // }
+    } else {
+      figma.closePlugin("Please select a component set");
+    }
+  }
+  figma.closePlugin();
+}
+
+iconScaling();
