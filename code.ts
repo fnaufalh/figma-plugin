e9efb1704c2d3ef01d1eafa884c4e829058bbd14
fnaufalh <fnaufalh@gmail.com>
2023-03-21 08:26:03 +0700

    try to union first then flatten

--- code.ts
+++ code.ts
@@ -2,49 +2,56 @@ const SIZE = [24, 16, 12];
 const select = figma.currentPage.selection;
 const selectedVectors: any = [];
 
+function removeGrid(clone: any) {
+  const grid = clone.findOne(
+    (node: any) => node.type === "INSTANCE" && node.name === "grid-icon"
+  ) as SceneNode;
+  grid?.remove();
+}
+
+function outlining(node: any, frame: FrameNode) {
+  if (node.strokeStyleId && node.fillStyleId) {
+    node.fills = [];
+    const stroke = node.outlineStroke() as VectorNode;
+    node.remove();
+    frame.appendChild(stroke);
+    node = stroke;
+  } else if (node.type === "LINE") {
+    console.log("out");
+  }
+  console.log(node.type);
+  selectedVectors.push(node);
+}
 function selectAllVector(frame: FrameNode) {
   for (const node of frame.children) {
     if (
       node.type === "VECTOR" ||
       node.type === "RECTANGLE" ||
       node.type === "ELLIPSE" ||
-      node.type === "BOOLEAN_OPERATION" ||
+      // node.type === "BOOLEAN_OPERATION" ||
       node.type === "LINE"
     ) {
       switch (node.type) {
-        case "BOOLEAN_OPERATION":
-          // const booleanOperation = node as BooleanOperationNode;
-          // booleanOperation.flatten();
+        // case "BOOLEAN_OPERATION":
+        // const booleanOperation = node as BooleanOperationNode;
+        // booleanOperation.flatten();
 
-          break;
+        // break;
+        case "LINE":
         case "VECTOR":
         case "RECTANGLE":
         case "ELLIPSE":
-        case "LINE":
         default:
-          if (node.strokeStyleId && node.fillStyleId) {
-            node.fills = [];
-            const stroke = node.outlineStroke() as VectorNode;
-            node.remove();
-            frame.appendChild(stroke);
-            selectedVectors.push(stroke);
-          } else {
-            selectedVectors.push(node);
-          }
+          outlining(node, frame);
           break;
       }
     }
-    const flatten = figma.flatten(selectedVectors);
-    flatten.name = "Vector";
   }
+  // const flatten = figma.flatten(selectedVectors);
+  // flatten.name = "Vector";
 }
 
 function iconScaling() {
-  if ("getPluginData" in figma.clientStorage) {
-    figma.closePlugin("v2");
-  } else {
-    figma.closePlugin("v1");
-  }
   for (const node of select) {
     if (node.type === "COMPONENT_SET") {
       const componentSet = select.find(
@@ -55,6 +62,7 @@ function iconScaling() {
       const component = componentSet.findOne(
         (node) => node.type === "COMPONENT"
       ) as ComponentNode;
+      removeGrid(component);
       const frame = componentSet.findOne(
         (node) => node.type === "COMPONENT"
       ) as FrameNode;
@@ -68,10 +76,6 @@ function iconScaling() {
       };
       for (const size of SIZE) {
         const clone = component.clone() as ComponentNode;
-        const grid = clone.findOne(
-          (node) => node.type === "INSTANCE" && node.name === "grid-icon"
-        ) as SceneNode;
-        grid?.remove();
         clone.resize(size, size);
         clone.name = "Size=" + size;
         componentSet.appendChild(clone);
